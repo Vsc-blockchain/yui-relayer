8f623755b0055283604d16772b07407132343910
Till Ziegler <tz@schoeneweide.tk>
2024-09-23 08:28:12 +0000

    set vsc specific power reduction

--- chains/tendermint/cmd/cmd.go
+++ chains/tendermint/cmd/cmd.go
@@ -4,9 +4,13 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/hyperledger-labs/yui-relayer/config"
 	"github.com/spf13/cobra"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	vsc "github.com/vsc-blockchain/core/types"
 )
 
 func TendermintCmd(m codec.Codec, ctx *config.Context) *cobra.Command {
+	sdk.DefaultPowerReduction = vsc.PowerReduction
+
 	cmd := &cobra.Command{
 		Use:   "tendermint",
 		Short: "manage tendermint configurations",
